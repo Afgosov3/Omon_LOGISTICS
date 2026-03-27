89395f77ccde9c6a11aa7b7fbe75d404313251cb
Afgosov3 <shamsiddin777zzz@gmail.com>
2026-03-27 12:43:43 +0500

    add jazzmin

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-)3i4m_0m!br4u*%@0(a3gyoik5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('127.0.0.1,localhost', '146.190.205.34', "api.crm.omonlogistics.uz").split(',')
+ALLOWED_HOSTS = os.getenv('146.190.205.34', "api.crm.omonlogistics.uz").split(',')
 
 
 # Application definition
